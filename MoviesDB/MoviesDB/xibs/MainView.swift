0a587c4c707650e1534f38b93c95bdfc131db7b9
Federico Torres <federico.torres@softvision.com>
2020-07-04 21:27:36 -0300

    show all data when no searching

--- MoviesDB/MoviesDB/xibs/MainView.swift
+++ MoviesDB/MoviesDB/xibs/MainView.swift
@@ -14,14 +14,15 @@ class MainView: UIView, UISearchBarDelegate, UITableViewDataSource, UITableViewD
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    let cellId = "CellView"
-    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
-        "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
-        "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
-        "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
-        "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
-
-    var filteredData: [String] = []
+    private let cellId = "CellView"
+    private let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+                        "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+                        "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+                        "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+                        "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+    
+    private var filteredData: [String] = []
+    private var isSearching : Bool = false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -41,36 +42,42 @@ class MainView: UIView, UISearchBarDelegate, UITableViewDataSource, UITableViewD
             view.backgroundColor = UIColor.clear
             addSubview(view)
             view.frame = self.bounds
-            self.backgroundColor = UIColor.cyan
         }
         
         searchBar.delegate = self
         tableView.dataSource = self
         tableView.delegate = self
-        //tableView.register(CellView.self, forCellReuseIdentifier: cellId)
         let myNib = UINib(nibName: "CellViewXib", bundle: nil)
         tableView.register(myNib, forCellReuseIdentifier: cellId)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! CellView
-       
-        cell.setTitle(title: data[indexPath.row])
-        //cell.setTitle(title: filteredData[indexPath.row])
-              return cell
+        
+        if(isSearching){
+            cell.setTitle(title: filteredData[indexPath.row])
+        } else {
+            cell.setTitle(title: data[indexPath.row])
+        }
+        
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        if(isSearching){
+            return filteredData.count
+        } else {
+            return data.count
+        }
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
-                  return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
-              }
-        dump(filteredData)
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        isSearching = !searchText.isEmpty
         tableView.reloadData()
     }
     
-
+    
 }
